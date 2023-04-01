b0036302f019dbe5803c7b9dc7f4e1113ee1644c
Ashadi-Fajarrohman <fajarrohmanashadi@gmail.com>
2023-04-01 11:52:32 +0700

    Update .gitignore

--- ashtech/blog/views.py
+++ ashtech/blog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'blog/base.html')
+    return render(request, 'blog/homepage.html')
